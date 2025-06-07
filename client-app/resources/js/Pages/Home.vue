060785f74c65999497889b95fc78a8c6296eab4c
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2025-06-08 01:47:25 +0900

    Set up Inertia.js with Vue 3 and create initial Home page

---
+++ client-app/resources/js/Pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Inertiaでのホーム画面</h1>
+        <p>認証なしのInertia-Vue構成です。</p>
+    </div>
+</template>
+
+<script setup>
+</script>
---
+++ client-app/resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Inertia App</title>
+    @vite('resources/js/app.js')
+    @inertiaHead
+</head>
+<body>
+@inertia
+</body>
+</html>
